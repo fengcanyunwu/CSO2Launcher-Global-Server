37a3ef03e9dd667a2bca7dde76d792777376b40e
Yusuf <36341134+izmoxkawaii@users.noreply.github.com>
2021-02-10 18:29:31 +0300

    LauncherSettings
    LauncherSettings

---
+++ Launcher/LauncherSettings/LauncherSettings.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Launcher
+{
+    partial class LauncherSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LauncherSettings));
+            this._Close = new System.Windows.Forms.PictureBox();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.Label = new System.Windows.Forms.Label();
+            this.Uninstall = new System.Windows.Forms.Button();
+            this.Repair = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this._Close)).BeginInit();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _Close
+            // 
+            this._Close.BackColor = System.Drawing.Color.Lime;
+            this._Close.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("_Close.BackgroundImage")));
+            this._Close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this._Close.Location = new System.Drawing.Point(772, 2);
+            this._Close.Name = "_Close";
+            this._Close.Size = new System.Drawing.Size(20, 20);
+            this._Close.TabIndex = 11;
+            this._Close.TabStop = false;
+            this._Close.Click += new System.EventHandler(this._Close_Click);
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.Lime;
+            this.TopPanel.Controls.Add(this.Label);
+            this.TopPanel.Controls.Add(this._Close);
+            this.TopPanel.Location = new System.Drawing.Point(0, 1);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(803, 102);
+            this.TopPanel.TabIndex = 12;
+            this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            this.TopPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.BackColor = System.Drawing.Color.Lime;
+            this.Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label.ForeColor = System.Drawing.Color.White;
+            this.Label.Location = new System.Drawing.Point(3, 3);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(68, 20);
+            this.Label.TabIndex = 15;
+            this.Label.Text = "Settings";
+            // 
+            // Uninstall
+            // 
+            this.Uninstall.BackColor = System.Drawing.Color.Lime;
+            this.Uninstall.BackgroundImage = global::Launcher.Properties.Resources.buttonUpdate;
+            this.Uninstall.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Uninstall.FlatAppearance.BorderSize = 0;
+            this.Uninstall.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Uninstall.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Uninstall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Uninstall.Font = new System.Drawing.Font("Impact", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Uninstall.ForeColor = System.Drawing.Color.White;
+            this.Uninstall.Location = new System.Drawing.Point(402, 469);
+            this.Uninstall.Name = "Uninstall";
+            this.Uninstall.Size = new System.Drawing.Size(385, 27);
+            this.Uninstall.TabIndex = 13;
+            this.Uninstall.Text = "Güncellemeyi Sil";
+            this.Uninstall.UseVisualStyleBackColor = false;
+            this.Uninstall.Click += new System.EventHandler(this.Uninstall_Click);
+            // 
+            // Repair
+            // 
+            this.Repair.BackColor = System.Drawing.Color.Lime;
+            this.Repair.BackgroundImage = global::Launcher.Properties.Resources.button;
+            this.Repair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Repair.FlatAppearance.BorderSize = 0;
+            this.Repair.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Repair.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Repair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Repair.Font = new System.Drawing.Font("Impact", 11.25F);
+            this.Repair.ForeColor = System.Drawing.Color.White;
+            this.Repair.Location = new System.Drawing.Point(12, 469);
+            this.Repair.Name = "Repair";
+            this.Repair.Size = new System.Drawing.Size(385, 27);
+            this.Repair.TabIndex = 15;
+            this.Repair.Text = "Güncellemeyi Tahmir ET";
+            this.Repair.UseVisualStyleBackColor = false;
+            this.Repair.Click += new System.EventHandler(this.Repair_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Launcher.Properties.Resources.font;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 410);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(216, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(391, 32);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Katkıda Bulunan Kullanıcılar";
+            this.label1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(36, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(355, 19);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "1.Sunucu Desteği ve Geliştirici: KouKouChan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(36, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(283, 19);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "2. Kore Dili Desteği ve Yönetici Hezz";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(36, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(245, 19);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "3. Tayvan Dil Desteği: 藍色 肥貓";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Location = new System.Drawing.Point(36, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 19);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "4.XXXXX";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Black;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Location = new System.Drawing.Point(36, 268);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 19);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "5.XXXXX";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Black;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label8.Location = new System.Drawing.Point(36, 297);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 19);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "6.XXXXX";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Black;
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label9.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label9.Location = new System.Drawing.Point(36, 327);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 19);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "7.XXXXX";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Black;
+            this.label11.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label11.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label11.Location = new System.Drawing.Point(36, 356);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 19);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "8.XXXXX";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Black;
+            this.label12.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label12.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label12.Location = new System.Drawing.Point(36, 385);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(77, 19);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "9.XXXXX";
+            // 
+            // LauncherSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Launcher.Properties.Resources._5;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(799, 508);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Repair);
+            this.Controls.Add(this.Uninstall);
+            this.Controls.Add(this.TopPanel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LauncherSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Settings";
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.Load += new System.EventHandler(this.LauncherSettings_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this._Close)).EndInit();
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox _Close;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Button Uninstall;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Button Repair;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
---
+++ Launcher/LauncherSettings/LauncherSettings.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class LauncherSettings : Form
+    {
+        main MainView;
+        public LauncherSettings(main Mainform)
+        {
+            MainView = Mainform;
+            InitializeComponent();
+        }
+        private void _Close_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        private void LauncherSettings_Load(object sender, EventArgs e)
+        {
+            _Close.BackColor = Color.Transparent;
+            Uninstall.BackColor = Color.Transparent;
+            Repair.BackColor = Color.Transparent;
+            Label.BackColor = Color.Transparent;
+
+            Label.Parent = TopPanel;
+            _Close.Parent = TopPanel;
+            
+
+            TopPanel.BackColor = Color.Transparent;
+        }
+        /*******************************
+            handle the moving window
+        /********************************/
+        int mouseX, mouseY = 0;
+        bool mouseDown;
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+        }
+        private void TopPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                mouseX = MousePosition.X - 170; //edge
+                mouseY = MousePosition.Y;
+
+                this.SetDesktopLocation(mouseX, mouseY);
+            }
+        }
+        private void Repair_Click(object sender, EventArgs e)
+        {
+            MainView.PerformRepair();
+            this.Hide();
+        }
+        private void Uninstall_Click(object sender, EventArgs e)
+        {
+
+            const string message = "Oyun Veya Son Güncelleme Kaldırılacaktır Bundan Emin Misiniz!";
+            const string caption = "Oyunu Kaldırmak İstediğinizden Emin Misiniz?";
+            var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                MainView.PerformUninstall();
+                MainView.PerformRepair();
+                this.Hide();
+            }
+            else if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Transparent;
+        }
+
+        private void TopPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+    }
+}
