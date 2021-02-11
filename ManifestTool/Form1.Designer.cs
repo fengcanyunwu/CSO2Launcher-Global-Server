5bb46b8bf5b9acedc473aa60a8f3f420276297ff
Yusuf <36341134+izmoxkawaii@users.noreply.github.com>
2021-02-12 02:52:59 +0300

    Update Form1.Designer.cs

--- ManifestTool/Form1.Designer.cs
+++ ManifestTool/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ManifestTool
+namespace ManifestTool
 {
     partial class Form1
     {
@@ -38,45 +38,50 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 663);
+            this.progressBar1.Location = new System.Drawing.Point(1, 82);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(1222, 23);
+            this.progressBar1.Size = new System.Drawing.Size(531, 10);
             this.progressBar1.TabIndex = 0;
             // 
             // DirectoryPath
             // 
-            this.DirectoryPath.Location = new System.Drawing.Point(12, 475);
+            this.DirectoryPath.Location = new System.Drawing.Point(1, 98);
             this.DirectoryPath.Name = "DirectoryPath";
-            this.DirectoryPath.Size = new System.Drawing.Size(1065, 20);
+            this.DirectoryPath.Size = new System.Drawing.Size(531, 20);
             this.DirectoryPath.TabIndex = 1;
             this.DirectoryPath.Text = "Target directory";
             // 
             // Open
             // 
-            this.Open.Location = new System.Drawing.Point(1083, 472);
+            this.Open.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Open.BackgroundImage")));
+            this.Open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Open.ForeColor = System.Drawing.Color.Black;
+            this.Open.Location = new System.Drawing.Point(178, 24);
             this.Open.Name = "Open";
-            this.Open.Size = new System.Drawing.Size(65, 23);
+            this.Open.Size = new System.Drawing.Size(171, 41);
             this.Open.TabIndex = 2;
-            this.Open.Text = "Open";
+            this.Open.Text = "Select Folder";
             this.Open.UseVisualStyleBackColor = true;
             this.Open.Click += new System.EventHandler(this.button1_Click);
             // 
             // Logging
             // 
-            this.Logging.Location = new System.Drawing.Point(12, 501);
+            this.Logging.Location = new System.Drawing.Point(1, 124);
             this.Logging.Name = "Logging";
             this.Logging.ReadOnly = true;
-            this.Logging.Size = new System.Drawing.Size(1222, 156);
+            this.Logging.Size = new System.Drawing.Size(531, 406);
             this.Logging.TabIndex = 3;
             this.Logging.Text = "";
             // 
             // Start
             // 
-            this.Start.Location = new System.Drawing.Point(1169, 472);
+            this.Start.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Start.BackgroundImage")));
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Start.Location = new System.Drawing.Point(355, 24);
             this.Start.Name = "Start";
-            this.Start.Size = new System.Drawing.Size(65, 23);
+            this.Start.Size = new System.Drawing.Size(171, 41);
             this.Start.TabIndex = 4;
-            this.Start.Text = "Start";
+            this.Start.Text = "Create Update File";
             this.Start.UseVisualStyleBackColor = true;
             this.Start.Click += new System.EventHandler(this.Start_Click);
             // 
@@ -85,9 +90,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.BackgroundImage = global::ManifestTool.Properties.Resources.a;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1246, 698);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(534, 532);
             this.Controls.Add(this.Start);
             this.Controls.Add(this.Logging);
             this.Controls.Add(this.Open);
